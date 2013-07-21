ea96d11e68b00b613107be2a97ae4fbeed42b277
chadrien <chadrien@chadrien.fr>
2013-07-21 14:08:22 +0200

    Enhanced cache management

--- toggl/commands/manage_projects_window.py
+++ toggl/commands/manage_projects_window.py
@@ -14,46 +14,35 @@ class ManageProjectsCommand(sublime_plugin.WindowCommand):
     projects      = None
 
     def run(self):
-        self.create_workspace_api_client()
-        self.create_project_api_client()
-
         if Cache.retrieve('workspaces') is None:
-            Cache.store('workspaces', self.get_workspaces())
+            self.create_workspace_api_client()
+            Cache.store('workspaces', self.workspace_api.get_workspaces())
 
         self.workspaces = Cache.retrieve('workspaces')
 
         show_palette(self.window, ['Workspace: ' + workspace['name'] for workspace in self.workspaces], self.chosen_workspace)
-
-    def create_workspace_api_client(self):
-        if Cache.retrieve('workspace_api') is None:
-            workspace_api = TogglWorkspaceApi()
-            workspace_api.authenticate(get_user_api_token())
-            Cache.store('workspace_api', workspace_api)
-
-        self.workspace_api = Cache.retrieve('workspace_api')
-
-    def create_project_api_client(self):
-        if Cache.retrieve('project_api') is None:
-            project_api = TogglProjectApi()
-            project_api.authenticate(get_user_api_token())
-            Cache.store('project_api', project_api)
-
-        self.project_api = Cache.retrieve('project_api')
-
-    def get_workspaces(self):
-        workspaces = self.workspace_api.get_workspaces()
-        return workspaces
 
     def chosen_workspace(self, workspace_pick):
         if workspace_pick is -1:
             return
 
         if Cache.retrieve('projects') is None:
+            self.create_project_api_client()
             Cache.store('projects', self.project_api.get_workspace_projects(self.workspaces[workspace_pick]['id']))
 
         self.projects = Cache.retrieve('projects')
 
         show_palette(self.window, ['Create new project'] + [project['name'] for project in self.projects], lambda project_pick: self.chosen_project(project_pick, self.workspaces[workspace_pick]['id']))
+
+    def create_workspace_api_client(self):
+        if self.workspace_api is None:
+            self.workspace_api = TogglWorkspaceApi()
+            self.workspace_api.authenticate(get_user_api_token())
+
+    def create_project_api_client(self):
+        if self.project_api is None:
+            self.project_api = TogglProjectApi()
+            self.project_api.authenticate(get_user_api_token())
 
     def chosen_project(self, project_pick, workspace_pick):
         if project_pick is -1:
@@ -61,6 +50,7 @@ class ManageProjectsCommand(sublime_plugin.WindowCommand):
 
         if project_pick is 0:
             self.window.show_input_panel('New project\'s name:', '', lambda project_name: self.create_new_project(workspace_pick, project_name), None, None)
+            return
 
         project_id = self.projects[project_pick - 1]['id']
         show_palette(self.window, ['Rename'], lambda action: self.chosen_project_action(action, project_id))
@@ -72,6 +62,7 @@ class ManageProjectsCommand(sublime_plugin.WindowCommand):
             sublime.error_message('Error creating project.')
         else:
             sublime.status_message('Project ' + project_name + ' created.')
+            Cache.store('projects', Cache.retrieve('projects') + [project])
 
     def chosen_project_action(self, project_action, project_id):
         if project_action is -1:
@@ -81,9 +72,15 @@ class ManageProjectsCommand(sublime_plugin.WindowCommand):
             self.window.show_input_panel('New name:', '', lambda new_project_name: self.update_project_name(new_project_name, project_id), None, None)
 
     def update_project_name(self, new_project_name, project_id):
-        project = self.project_api.update(project_id, {'name': new_project_name})
+        updated_project = self.project_api.update(project_id, {'name': new_project_name})
 
-        if not project:
+        if not updated_project:
             sublime.error_message('Error renaming project.')
         else:
             sublime.status_message('Project renamed to ' + new_project_name)
+            projects = Cache.retrieve('projects')
+            for key, project in enumerate(projects):
+                if project['id'] == updated_project['id']:
+                    projects[key] = updated_project
+                    break
+            Cache.store('projects', projects)
