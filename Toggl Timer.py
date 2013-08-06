cad2a2e1f5280c5e92961d48abe5c3597e303994
chadrien <chadrien@chadrien.fr>
2013-08-06 22:13:54 +0200

    Moved ssl loading and load urllib only after ssl is loaded

--- Toggl Timer.py
+++ Toggl Timer.py
@@ -1,6 +1,3 @@
-from .toggl.utils.ssl import load_ssl
 from .toggl.commands.manage_projects_window import ManageProjectsCommand
 from .toggl.commands.manage_time_entries_window import ManageTimeEntriesCommand
 from .toggl.commands.clear_cache import ClearCacheCommand
-
-load_ssl()
