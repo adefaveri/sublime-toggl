575b689ca327378d730ae4a12885cfa05801f2e1
chadrien <chadrien@chadrien.fr>
2013-09-10 21:40:23 +0200

    CS - PEP8

--- toggl/utils/toggl_api/toggl_project_api.py
+++ toggl/utils/toggl_api/toggl_project_api.py
@@ -1,7 +1,8 @@
 from .toggl_base_api import TogglBaseApi
 
+
 class TogglProjectApi(TogglBaseApi):
-    WORKSPACE_PROJECTS_LIST_URL  = 'https://www.toggl.com/api/v8/workspaces/%s/projects'
+    WORKSPACE_PROJECTS_LIST_URL = 'https://www.toggl.com/api/v8/workspaces/%s/projects'
     WORKSPACE_PROJECT_CREATE_URL = 'https://www.toggl.com/api/v8/projects'
     WORKSPACE_PROJECT_UPDATE_URL = 'https://www.toggl.com/api/v8/projects/%s'
 
