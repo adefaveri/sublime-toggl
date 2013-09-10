28c938a4af18ba12986a8726c22867d5dd9db99b
chadrien <chadrien@chadrien.fr>
2013-09-10 21:19:39 +0200

    CS - PEP8

--- toggl/utils/toggl_api/toggl_base_api.py
+++ toggl/utils/toggl_api/toggl_base_api.py
@@ -1,16 +1,16 @@
 from base64 import b64encode
 import json
-import imp
 
 from ..ssl import load_ssl
 load_ssl()
 
 from urllib.request import *
 
+
 class TogglBaseApi():
-    SESSION_URL                = 'https://www.toggl.com/api/v8/sessions'
+    SESSION_URL = 'https://www.toggl.com/api/v8/sessions'
     SESSION_COOKIE_FROM_HEADER = 'Set-cookie'
-    SESSION_COOKIE_TO_HEADER   = 'Cookie'
+    SESSION_COOKIE_TO_HEADER = 'Cookie'
 
     session_cookie = None
 
@@ -32,11 +32,11 @@ class TogglBaseApi():
 
         return {'Authorization': 'Basic '+encoded_phrase.decode()}
 
-    def request(self, url, datas = None, headers = {}, method = 'GET'):
+    def request(self, url, datas=None, headers={}, method='GET'):
         if self.is_logged():
             headers[self.SESSION_COOKIE_TO_HEADER] = self.session_cookie
 
-        request  = Request(url, datas, headers, None, False, method)
+        request = Request(url, datas, headers, None, False, method)
         response = urlopen(request)
 
         if response.status is not 200:
